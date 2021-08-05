e8da35dfe50cf00207fa8b1ede48ec86e7031385
Prateek Goyal <akshat.goyal_cs17@gla.ac.in>
2021-08-05 15:33:25 +0530

    Final commit....

--- java_xml_dom_parser/src/demp/java/dom/XmlJDomParser.java
+++ java_xml_dom_parser/src/demp/java/dom/XmlJDomParser.java
@@ -17,7 +17,7 @@ public class XmlJDomParser {
          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
          Document doc = dBuilder.parse(inputFile);
-         doc.getDocumentElement().normalize();
+         doc.getDocumentElement();
          System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
          NodeList nList = doc.getElementsByTagName("student");
          
@@ -29,9 +29,8 @@ public class XmlJDomParser {
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                Element eElement = (Element) nNode;
                System.out.println("Student roll no : "  + eElement.getAttribute("rollno"));
-                   //System.out.println("First Name : " + eElement.getElementsByTagName("firstname") .item(0) .getTextContent());
+               System.out.println("First Name : " + eElement.getElementsByTagName("firstname") .item(0) .getTextContent());
                System.out.println("Last Name : "  + eElement .getElementsByTagName("lastname").item(0).getTextContent());
-               System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
                System.out.println("Marks : " + eElement.getElementsByTagName("marks").item(0).getTextContent());
             }
          }
